9cd9855d2f25667f4b7bb57ec8538b58405a902e
chris varner <christopher.varner@gmail.com>
2014-04-25 23:27:11 -0500

    latest

---
+++ elpa/popup-20140207.1702/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21332 18353
+;;;;;;  614000 0))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
